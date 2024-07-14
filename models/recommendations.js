8cbc1851a20852a2d776c789d3d08820885aca78
shashwat2712 <shashwatsaivyas@gmail.com>
2024-07-14 18:30:18 +0530

    resolved the error in the feature of recommendations

--- models/recommendations.js
+++ models/recommendations.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');// Ensure this is the correct path
 
 const recommendationSchema = new mongoose.Schema({
     recommendation : [],
+    recommendations : [],
 });
 
 const Recommendations = mongoose.model('recommendations', recommendationSchema);
